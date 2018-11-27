0d76495547361aa4fda823e31d41c350484ceaf2
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-27 13:26:08 +0000

    first commit

---
+++ src/Airport.js
@@ -0,0 +1,24 @@
+function Airport(weather) {
+  this._weather = typeof weather !== 'undefined' ? weather : new Weather();
+  this._hangar = [];
+}
+
+Airport.prototype.isStormy = function() {
+  return this._weather.isStormy();
+};
+
+Airport.prototype.clearForLanding = function (plane) {
+  if(this.isStormy()) {
+      throw new Error('cannot land during storm');
+  }
+  this._hangar.push(plane);
+};
+
+Airport.prototype.clearForTakeOff = function () {
+  if(this.isStormy()) {
+    throw new Error('cannot takeoff during storm');
+  }
+  this._hangar = [];
+};
+
+Airport.prototype.planes = function(){ return this._hangar; };
